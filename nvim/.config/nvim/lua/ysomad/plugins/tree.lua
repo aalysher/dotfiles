fe343a82b8744527522c410146d91d112f20e3b8
Isken <isken@18peaches.com>
2024-08-07 21:13:23 +0600

    change nvim tree

--- nvim/.config/nvim/lua/ysomad/plugins/tree.lua
+++ nvim/.config/nvim/lua/ysomad/plugins/tree.lua
@@ -1,3 +1,18 @@
+-- return {
+--   "kyazdani42/nvim-tree.lua",
+--   requires = { "kyazdani42/nvim-web-devicons" },
+--   config = function()
+--     vim.g.loaded_netrw = 1
+--     vim.g.loaded_netrwPlugin = 1
+--     require("nvim-tree").setup({
+--       filters = {
+--         dotfiles = true,
+--       }
+--     })
+--     vim.keymap.set("n", "<A-1>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
+--   end
+-- }
+
 return {
   "kyazdani42/nvim-tree.lua",
   requires = { "kyazdani42/nvim-web-devicons" },
@@ -7,10 +22,33 @@ return {
     require("nvim-tree").setup({
       filters = {
         dotfiles = true,
-      }
+      },
+      view = {
+        side = "left",
+        width = 30,
+      },
     })
-    vim.keymap.set("n", "<A-1>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
+
+    local function is_nvim_tree_open()
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        if vim.api.nvim_buf_get_name(buf):match("NvimTree") then
+          return true
+        end
+      end
+      return false
+    end
+
+    local function toggle_tree()
+      if is_nvim_tree_open() then
+        vim.cmd("NvimTreeClose")
+      else
+        vim.cmd("NvimTreeOpen")
+        vim.cmd("NvimTreeFindFile")
+      end
+    end
+
+    vim.keymap.set("n", "<A-1>", toggle_tree, { desc = "Toggle NvimTree and focus on current file" })
   end
 }
-
 
