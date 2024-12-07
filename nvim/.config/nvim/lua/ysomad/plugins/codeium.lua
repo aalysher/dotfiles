3102cec44b65a4ae7fea27649fcec703d502470f
Isken <isken@18peaches.com>
2024-12-08 03:39:58 +0600

    upd

--- nvim/.config/nvim/lua/ysomad/plugins/codeium.lua
+++ nvim/.config/nvim/lua/ysomad/plugins/codeium.lua
@@ -2,13 +2,10 @@ return {
   "Exafunction/codeium.vim",
   event = 'BufEnter',
   config = function()
-    vim.g.codeium_model = 'anthropic'
+    vim.g.codeium_anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
+    vim.g.codeium_model = 'claude-3-5-sonnet'
     vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true, desc = "Codeium Accept" })
     vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true, desc = "Codeium Clear" })
     vim.keymap.set('i', '<C-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true, desc = "Codeium Cycle Completions Next" })
-
-    vim.g.codeium_filetypes = {
-      markdown = false,
-    }
   end
 }
--- nvim/.config/nvim/lua/ysomad/plugins/dap.lua
+++ nvim/.config/nvim/lua/ysomad/plugins/dap.lua
@@ -9,10 +9,41 @@ return {
     local dap = require("dap")
     local dapui = require("dapui")
     local dap_go = require("dap-go")
+    dap_go.setup({
+      delve = {
+        path = "dlv",
+        initialize_timeout_sec = 20,
+        build_flags = "-gcflags='all=-N -l'",
+      },
+    })
 
-    dap_go.setup()
+    dap.configurations.go = {
+      {
+        type = "go",
+        name = "Debug Current Package",
+        request = "launch",
+        mode = "debug",
+        program = "${fileDirname}",
+        buildFlags = "-gcflags='all=-N -l'"
+      },
+      {
+        type = "go",
+        name = "Debug Main Package",
+        request = "launch",
+        mode = "debug",
+        program = "${workspaceFolder}/cmd/main.go",
+        buildFlags = "-gcflags='all=-N -l'"
+      },
+      {
+        type = "go",
+        name = "Debug Test",
+        request = "launch",
+        mode = "test",
+        program = "${fileDirname}"
+      }
+    }
+
     dapui.setup()
-
     dap.listeners.after.event_initialized["dapui_config"] = function()
       dapui.open()
     end
@@ -26,19 +57,13 @@ return {
     vim.keymap.set("n", "<F21>", dap.continue, { desc = "DAP continue" })
     vim.keymap.set("n", "<F8>", dap.step_over, { desc = "DAP step over" })
     vim.keymap.set("n", "<F9>", dap.step_out, { desc = "DAP step out" })
-    -- vim.keymap.set("n", "<F2>", dap.step_into, { desc = "DAP step into" })
     vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "DAP toggle breakpoint" })
     vim.keymap.set("n", "<leader>dr", dap.repl.open, { desc = "DAP open REPL" })
-    -- vim.keymap.set("n", "<leader>dt", dap_go.debug_test, { desc = "DAP debug test" })
-
     vim.keymap.set("n", "<leader>B", function ()
       dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
     end, { desc = "DAP set breakpoint with condition" })
-
     vim.keymap.set("n", "<leader>lp", function ()
       dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
     end, { desc = "DAP set breakpoint with log msg" })
-
   end
 }
-
