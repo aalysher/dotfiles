322d572ccb7300eecc44e343e115eeb6a6fbc047
Aalysher <aalysher@gmail.com>
2024-12-08 03:39:58 +0600

    upd

--- nvim/.config/nvim/lua/ysomad/keymaps.lua
+++ nvim/.config/nvim/lua/ysomad/keymaps.lua
@@ -14,12 +14,7 @@ map("n", "<Esc>", "<cmd>noh<CR>", { desc = "General Clear highlights" })
 map("v", "K", ":m '<-2<CR>gv=gv")
 map("v", "J", ":m '>+1<CR>gv=gv")
 
-
-map("v", "K", ":m '<-2<CR>gv=gv")
-map("v", "J", ":m '>+1<CR>gv=gv")
-
 map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
 
 -- tmux navigator
 map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>")
