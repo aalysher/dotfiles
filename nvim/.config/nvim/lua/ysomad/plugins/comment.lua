41bbf73d2d03d6a85be271d897bc5a5eeb3c4c20
Aalysher <aalysher@gmail.com>
2024-12-13 20:52:16 +0600

    upd

--- nvim/.config/nvim/lua/ysomad/plugins/comment.lua
+++ nvim/.config/nvim/lua/ysomad/plugins/comment.lua
@@ -7,13 +7,13 @@ return {
     -- Маппинги для комментирования
     vim.keymap.set("n", "<C-_>", function()
       require("Comment.api").toggle.linewise.current()
-    end, { desc = "Comment Toggle" })
+    end, { desc = "Comment Toggle", noremap = true, silent = true })
 
     vim.keymap.set(
       "v",
       "<C-_>",
       "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-      { desc = "Comment Toggle" }
+      { desc = "Comment Toggle", noremap = true, silent = true }
     )
   end
 }
