0acfc9fc80ba1d632c0f9fdc21e651bd42c83b6e
Aalysher <aalysher@gmail.com>
2024-12-29 06:22:00 +0600

    upd

--- nvim/.config/nvim/lua/ysomad/plugins/telescope.lua
+++ nvim/.config/nvim/lua/ysomad/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    'nvim-telescope/telescope-ui-select.nvim',
   },
   config = function ()
     local telescope = require("telescope")
@@ -30,10 +31,17 @@ return {
             }
           }
         }
+      },
+      extensions = {
+        ['ui-select'] = {
+          require('telescope.themes').get_dropdown({
+          })
+        }
       }
     })
 
     telescope.load_extension("fzf")
+    telescope.load_extension("ui-select")
 
     vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telescope Find files" })
     vim.keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Telescope Live grep" })
@@ -53,6 +61,7 @@ return {
     end, { desc = "Grep string" })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "List help tags" })
     vim.keymap.set('n', '<leader>fx', builtin.treesitter, { desc = "List tresitter funcs, vars"})
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
   end
 }
 
