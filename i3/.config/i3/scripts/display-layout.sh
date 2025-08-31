467c53cd4860db0e6c090aa7d607e1563c7338d1
Aalysher <aalysher@gmail.com>
2025-09-01 00:59:46 +0600

    fix status bar and remapping caps lock to control

--- i3/.config/i3/scripts/display-layout.sh
+++ i3/.config/i3/scripts/display-layout.sh
@@ -2,11 +2,17 @@
 # Скрипт для добавления информации о раскладке в i3status
 
 # Путь к конфигурационному файлу i3status
-I3STATUS_CONFIG="$HOME/.config/i3/i3status.conf"
+I3STATUS_CONFIG="$HOME/.config/i3/i3status/i3status.conf"
 
 i3status --config $I3STATUS_CONFIG | while :
 do
     read line
-    LG=$(setxkbmap -query | awk '/layout/{print $2}')
+    LAYOUTS=$(setxkbmap -query | awk '/layout/{print $2}')
+    LED_STATUS=$(xset -q | awk '/LED mask/{print $10}')
+    if [ "$LED_STATUS" = "00000000" ]; then
+        LG=$(echo "$LAYOUTS" | cut -d ',' -f 1)
+    else
+        LG=$(echo "$LAYOUTS" | cut -d ',' -f 2)
+    fi
     echo "⌨️ $LG | $line" || exit 1
 done
